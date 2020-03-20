ab051d55a95766e09b0ed32684a97052910a75bb
lucas-goes <lucasdegoes@outlook.com.br>
2020-03-19 23:30:14 -0300

    adicionando botao chaveador com efeito

--- preparando-projeto/javascript/app.js
+++ preparando-projeto/javascript/app.js
@@ -1,6 +1,9 @@
-var alternador = document.querySelector('.js-botao-chaveador');
-
-alternador.onclick = function(){
+function abreMenu(x) {
     var menu = document.querySelector('.js-menu');
+    
     menu.classList.toggle('menu--exibindo');
-}
+  }
+
+function animacao(x) {
+    x.classList.toggle("change");
+  }
